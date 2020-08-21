a43aa15a51b68164b7225479a5a56da355243304
alefanfi <alefanfi@gmail.com>
2020-08-21 12:46:57 -0400

    Working on AddEntryTest

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BookieTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BookieTest.java
@@ -1,9 +1,6 @@
 package org.apache.bookkeeper.bookie;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
-@RunWith(value = Parameterized.class)
 public class BookieTest {
 
 
