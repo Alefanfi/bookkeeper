ebdbf2014b8712e5634c2747f59650def12387ae
alefanfi <alefanfi@gmail.com>
2020-09-05 06:12:57 -0400

    check test format

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/FormatTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/FormatTest.java
@@ -8,9 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
@@ -22,19 +20,20 @@ public class FormatTest {
     private final Boolean isInteractive;
     private final Boolean force;
     private final Object expected;
-    private final Boolean writeFile;
+
+    private final Boolean setDir;
 
     private File myFile;
 
     private static String[] journalDir;
 
-    public FormatTest(ServerConfiguration conf, Boolean isInteractive, Boolean force, Object expected, Boolean writeFile){
+    public FormatTest(ServerConfiguration conf, Boolean isInteractive, Boolean force, Object expected, Boolean setDir){
 
         this.conf = conf;
         this.isInteractive = isInteractive;
         this.force = force;
         this.expected = expected;
-        this.writeFile = writeFile;
+        this.setDir = setDir;
     }
 
     @Parameterized.Parameters
@@ -47,20 +46,22 @@ public class FormatTest {
         dir.mkdir();
         new File(dir, "test.txt");
         new File(dir, "tested.txt");
-        File dir2 = new File("dir2");
+       /* File dir2 = new File("dir2");
         dir2.mkdir();
-        new File(dir2, "test2.txt");
-
+        new File(dir2, "test2.txt");**/
 
         return Arrays.asList(new Object[][]{
 
-                {null, true, false, NullPointerException.class, false},
+                {null, true, false, NullPointerException.class, true},
                 {new ServerConfiguration(), false, true, true, true},
                 {new ServerConfiguration(), false, false, false, true},
                 {server, false, true, true, true},
                 {server, false, false, true, true},
                 {server, true, false, true, true},
-                {server, false, false, true, false},
+                {server, true, true, true, true},
+                {server, false, false, true,  true},
+                {new ServerConfiguration(), false, false, false, false},
+                {new ServerConfiguration(), true, true, true, false}, //Are you sure to format?
 
                 //{new ServerConfiguration(), true, false, true},
                 //{new ServerConfiguration(), true, true, true}
@@ -72,8 +73,11 @@ public class FormatTest {
     public void afterFunction() throws IOException {
 
         //delete folder
-        FileUtils.deleteDirectory(new File("dir"));
-        FileUtils.deleteDirectory(new File("dir2"));
+        if(new File("dir").exists()) {
+            FileUtils.deleteDirectory(new File("dir"));
+        }else if(new File("dir2").exists()) {
+            FileUtils.deleteDirectory(new File("dir2"));
+        }
 
     }
 
@@ -82,7 +86,7 @@ public class FormatTest {
 
         Object result;
 
-        if(conf != null){
+        if(conf != null && setDir){
 
              conf.setLedgerDirNames(new String[]{"temp", "tester"});
              myFile =  new File("/tmp/bk-txn/test.txt");
@@ -90,13 +94,6 @@ public class FormatTest {
              try{
 
                  myFile.createNewFile();
-
-                 if(writeFile) {
-
-                     BufferedWriter writer = new BufferedWriter(new FileWriter(myFile));
-                     writer.write("Hello world!");
-
-                 }
 
              } catch (IOException e) {
 
